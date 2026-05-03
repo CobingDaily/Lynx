dc8769378c39d48f6b7dad25e2aa999b99d2ca19
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2026-05-03 19:27:47 +0200

    Std library: prelude higher order functions into repl

--- src/repl.ml
+++ src/repl.ml
@@ -13,6 +13,22 @@ let interpret (s: string) =
         Printf.printf "Error: %s\n" (Printexc.to_string e)
 ;;
 
+let prelude_std (src_code : string) =
+  let prelude_files = Sys.readdir "std" 
+    |> Array.to_list
+    |> List.map (fun name -> "std" ^ "/" ^ name)
+  in
+  let file_content_as_str filename =
+    let cin = open_in filename in
+    In_channel.input_all cin
+  in
+  let std_src =
+    let file_sources = List.map (file_content_as_str) prelude_files in
+    List.fold_right (fun file_str acc -> acc ^ "\n" ^ file_str) file_sources ""
+  in
+  std_src ^ "\n" ^ src_code
+;;
+
 let quit_input = function
     | "q" | "Q" | "quit" | "exit" -> true
     | _ -> false
@@ -23,7 +39,7 @@ let rec repl_loop () =
     Printf.printf ">>> ";
     let user_input = read_line () in
     if quit_input user_input then ()
-    else (interpret user_input; repl_loop ();)
+    else (interpret (prelude_std user_input); repl_loop ();)
 ;;
 
 let () = repl_loop ();;
